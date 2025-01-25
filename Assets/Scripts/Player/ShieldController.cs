6c12f7702c47d717290763eac868ec192181bc18
Jordi <jordiportasola@gmail.com>
2025-01-25 19:34:20 +0100

    Hero looking + shield looking

--- Assets/Scripts/Player/ShieldController.cs
+++ Assets/Scripts/Player/ShieldController.cs
@@ -10,6 +10,16 @@ public class ShieldController : MonoBehaviour
     private HeroController _heroController;
     private HeroInput _heroInput;
 
+    private SpriteRenderer MainArt { get; set; }
+
+    [field: SerializeField]
+    public GameObject shieldObject { get; private set; }
+
+    [SerializeField] private Sprite _upSprite;    // Sprite mirando hacia arriba
+    [SerializeField] private Sprite _downSprite;  // Sprite mirando hacia abajo
+    [SerializeField] private Sprite _sideSprite;  // Sprite mirando hacia los lados
+    private bool test = true;
+
     private void Awake()
     {
         // Configurar Singleton
@@ -22,6 +32,7 @@ public class ShieldController : MonoBehaviour
 
         _heroController = GetComponentInParent<HeroController>();
         _heroInput = GetComponentInParent<HeroInput>();
+        MainArt = shieldObject.GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
@@ -50,7 +61,49 @@ public class ShieldController : MonoBehaviour
         // Aplica la rotaci�n para que el escudo apunte hacia el rat�n
         shield.SetPositionAndRotation(shieldPosition, Quaternion.Euler(0, 0, angle));
 
+        UpdateSpriteDirection(angle);
     }
+
+    private void UpdateSpriteDirection(float angle)
+    {
+        if (angle > 45 && angle <= 135)
+        {
+            // Mirando hacia arriba
+            MainArt.sprite = _upSprite;
+            MainArt.sortingOrder = 9;
+            MainArt.transform.localRotation = Quaternion.Euler(0, 0, 270);
+           
+        }
+        else if (angle <= -45 && angle > -135)
+        {
+            // Mirando hacia abajo
+            MainArt.sprite = _downSprite;
+            MainArt.sortingOrder = 11;
+            MainArt.transform.localRotation = Quaternion.Euler(0, 0, 90);
+        }
+        else
+        {
+            MainArt.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            // Mirando hacia los lados
+            MainArt.sprite = _sideSprite;
+
+            // Determina si el escudo está mirando a la izquierda o derecha
+            if ((angle >= 140 && angle <= 180) || (angle <= -140 && angle >= -180))
+            {
+                MainArt.transform.localRotation = Quaternion.Euler(0, 0, 180);
+                MainArt.flipX = true; // Mirando a la izquierda
+                
+            }
+            else if ((angle >= 0 && angle <= 40) || (angle <= 0 && angle >= -40))
+            {
+                MainArt.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                MainArt.flipX = false; // Mirando a la derecha
+            }
+            MainArt.sortingOrder = 10;
+        }
+    }
+
+
 
     private Vector3 GetMousePosition()
     {
